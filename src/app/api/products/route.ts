0a0d97bc0d5d5b1183268117b62d438a922f9d10
aronhawkins-4 <ahawki18@asu.edu>
2023-08-10 12:25:57 -0500

    Work on headful version

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,53 +4,61 @@ import puppeteer from 'puppeteer';
 import * as cheerio from 'cheerio';
 
 export async function GET(request: NextRequest) {
-	try {
-		// Get URL Params and parse search term
-		const query = request.nextUrl.searchParams;
-		const search_term = query.get('search_term');
-		if (!search_term ?? typeof search_term !== 'string') {
-			return NextResponse.json([]);
-		}
+    try {
+        // Get URL Params and parse search term
+        const query = request.nextUrl.searchParams;
+        const search_term = query.get('search_term');
+        if (!search_term ?? typeof search_term !== 'string') {
+            return NextResponse.json([]);
+        }
 
-		// Open Puppeteer Browser
-		const browser = await puppeteer.launch({ headless: false });
-		const page = await browser.newPage();
-		await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-		await page.goto(`https://www.target.com/s?searchTerm=${search_term}`);
-		await page.evaluate(() => {
-			window.scrollBy(0, window.innerHeight * 100);
-		});
-		await page.waitForSelector('.ProductRecsWrapper-sc-b7g1ua-0 img', { timeout: 60_000 });
-		const html = await page.evaluate(() => {
-			return {
-				html: document.documentElement.innerHTML,
-			};
-		});
+        // Open Puppeteer Browser
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setUserAgent(
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        );
+        await page.goto(`https://www.target.com/s?searchTerm=${search_term}`, {
+            waitUntil: 'networkidle2',
+        });
+        await page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight * 100);
+        });
+        await page.waitForSelector('.ProductRecsWrapper-sc-b7g1ua-0 img', {
+            timeout: 60_000,
+        });
+        const html = await page.evaluate(() => {
+            return {
+                html: document.documentElement.innerHTML,
+            };
+        });
 
-		// Initialize Cheerio to parse html elements
-		const $ = cheerio.load(html.html);
+        // Initialize Cheerio to parse html elements
+        const $ = cheerio.load(html.html);
 
-		// Get Product Cards
-		const productCards = $('body').find('.styles__StyledCardWrapper-sc-z8946b-0');
-		let productArray: Product[] = [];
+        // Get Product Cards
+        const productCards = $('body').find(
+            '.styles__StyledCardWrapper-sc-z8946b-0'
+        );
+        let productArray: Product[] = [];
 
-		// Map over product cards and put information into Product objects which are pushed into productArray
-		productCards.map((i, el) => {
-			let newProduct: Product = {};
-			newProduct.imageSrc = $(el).find('source').attr('srcset');
-			newProduct.imageAlt = $(el).find('img').first().attr('alt');
-			newProduct.title = $(el).find('img').first().attr('alt');
-			newProduct.link = $(el).find('a').first().attr('href');
-			productArray.push(newProduct);
-		});
-		await browser.close();
-		if (productArray) {
-			return NextResponse.json(productArray);
-		} else {
-			return NextResponse.json([]);
-		}
-	} catch (error: any) {
-		console.log('ERROR_GET_PRODUCTS', error);
-		return new NextResponse('Internal Error', { status: 500 });
-	}
+        // Map over product cards and put information into Product objects which are pushed into productArray
+        productCards.map((i, el) => {
+            let newProduct: Product = {};
+            newProduct.imageSrc = $(el).find('source').attr('srcset');
+            newProduct.imageAlt = $(el).find('img').first().attr('alt');
+            newProduct.title = $(el).find('img').first().attr('alt');
+            newProduct.link = $(el).find('a').first().attr('href');
+            productArray.push(newProduct);
+        });
+        await browser.close();
+        if (productArray) {
+            return NextResponse.json(productArray);
+        } else {
+            return NextResponse.json([]);
+        }
+    } catch (error: any) {
+        console.log('ERROR_GET_PRODUCTS', error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
 }
